ab6ecdb1fe4e98ed9f25322dd7eb704a9534d091
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-03-07 09:17:11 -0500

    script trial frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,17 +1,11 @@
 dnf install nginx -y
 systemctl enable nginx
 systemctl start nginx
+cp expense.conf /etc/nginx/default.d/expense.conf
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-vim /etc/nginx/default.d/expense.conf
-proxy_http_version 1.1;
 
-location /api/ { proxy_pass http://localhost:8080/; }
 
-location /health {
-  stub_status on;
-  access_log off;
-}
 systemctl restart nginx
