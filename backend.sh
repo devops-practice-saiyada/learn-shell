5c17babc70aae4ef15db946101d9794cdf9c9ce0
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-03-07 11:59:51 -0500

    backend files-redirectors

--- backend.sh
+++ backend.sh
@@ -1,21 +1,24 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
+dnf module disable nodejs -y >/temp/expense.log
+dnf module enable nodejs:20 -y >/temp/expense.log
+dnf install nodejs -y >/temp/expense.log
 
-useradd expense
+useradd expense >/temp/expense.log
+cp backend.service /etc/systemd/system/backend.service >/temp/expense.log
 
-mkdir /app
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
-cd /app
-unzip /tmp/backend.zip
+rm -rf/app >/temp/expense.log
 
-cd /app
-npm install
+mkdir /app >/temp/expense.log
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip >/temp/expense.log
+cd /app >/temp/expense.log
+unzip /tmp/backend.zip >/temp/expense.log
 
-cp backend.service /etc/systemd/system/backend.service
+cd /app >/temp/expense.log
+npm install >/temp/expense.log
 
-systemctl daemon-reload
-systemctl enable backend
-systemctl start backend
-dnf install mysql -y
-mysql -h 44.204.95.67 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+
+
+systemctl daemon-reload >/temp/expense.log
+systemctl enable backend >/temp/expense.log
+systemctl start backend >/temp/expense.log
+dnf install mysql -y >/temp/expense.log
+mysql -h 44.204.95.67 -uroot -pExpenseApp@1 < /app/schema/backend.sql >/temp/expense.log
